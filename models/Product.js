aa1f5d65027024c7746e1806b11e6730bf03487a
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-03 12:15:29 +1030

    Completed Product Model'

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,16 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTETGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
   },
   {
     sequelize,
