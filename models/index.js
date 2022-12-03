86fa614dbd2fd7dcb1ab7a33316ec63d6bab3141
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-03 12:37:14 +1030

    Completed Model INDEX

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+
+//Catergory / Product Joins
+Category.hasMany(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+});
+
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag});
 
 module.exports = {
   Product,
