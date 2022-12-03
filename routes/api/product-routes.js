9e2d8cd03d702c8e9fc436921274268d7b9776f6
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-03 14:00:53 +1030

    Completed GET one Product ROUTE

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,10 +6,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category}, { model: Tag}]
+      include: [{ model: Category }, { model: Tag }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -18,9 +17,25 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findOne({
+      where: {id: req.params.id},
+      include: [
+        { model: Category }, 
+        { model: Tag }
+      ]
+    });
+
+    if (!productData) {
+      res.status(404).json({message: 'No Product with matching ID found'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
