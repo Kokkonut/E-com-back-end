9cc3a20d7d44754fd8d09d493febabc01ef83905
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-04 13:10:54 +1030

    Completed ALL Tag ROUTES

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,27 +2,83 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
-});
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: { model: Product }
+    });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+    res.status(200).json(tagData);
 
-router.post('/', (req, res) => {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+  
+// find a single tag by its `id`
+router.get('/:id', async(req, res) => {
+  try {
+    const tagData = await Tag.findOne({
+      where: {id: req.params.id},
+      include: { model: Product},
+    });
+
+    if (!tagData) {
+      res.status(404).json({message: 'No tag with m,atching ID found!'})
+    }
+
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   // create a new tag
-});
+router.post('/', async(req, res) => {
+  try{
+    const tagData = await Tag.create(req.body);
 
-router.put('/:id', (req, res) => {
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {id: req.params.id}
+    });
+
+    if(!tagData) {
+      res.status(404).json({message: 'No tag with m,atching ID found!'})
+    };
+
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+      const tagData = await Tag.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!tagData) {
+        res.status(404).json({message: 'No tag with m,atching ID found!'})
+      };
+
+      res.status(200).json({message: 'Tag Deleted'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
